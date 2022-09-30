28ffee6f214158bcb99febd02ac86453bb784541
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-09-30 16:37:14 +0100

    Task Complete
    When I ran pytest it kept on saying error on where I raised an exception using the raise value command

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError(f'number was {number_of_primes}. It should be more than 0')
+
     list = []
+    count=2
+
+
+    while number_of_primes!=0:
+        for i in range(2,count):
+            if(count%i==0):
+                break
+        else:
+            list.append(count)
+            number_of_primes-=1
+        count+=1
+
     return list
+
+print(primes(10))
